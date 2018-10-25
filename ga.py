5e3bd8a06529b6d0e99ec5a0e568bb7b7c5fd405
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2018-10-25 15:00:53 -0700

    Modifying fitness function

--- ga.py
+++ ga.py
@@ -8,20 +8,38 @@ def fitness_function(genome):
     # this will be our fitness
     #sum = np.sum(genome)
 
+    print('String being tested is')
+    print(genome)
+
+    print('Decoded string is')
+    print(decode_function(genome))
+
     #setting parameter values using genome
-    polyak = 0.7
+    polyak = 0.7 
     gamma = 0.98
     epochs_default = 70
-    
+    env = 'FetchPickAndPlace-v1'
+    logdir = '/tmp/openaitest1'
+
     #calling training to calculate number of epochs required to reach close to maximum success rate
-    epochs = train.launch('FetchPickAndPlace-v1', '/tmp/openaitest', epochs_default, 1, 0, 'future', 5, 1, polyak, gamma)
+    epochs = train.launch(env, logdir, epochs_default, 1, 0, 'future', 5, 1, polyak, gamma)
     #env, logdir, n_epochs, num_cpu, seed, replay_strategy, policy_save_interval, clip_return
 
     return epochs
 
+def decode_function(genome_partial):
+
+    prod = 0
+    for i in reversed(list(enumerate(genome_partial))):
+        if genome_partial[i] == false:
+            prod += 0
+        else:
+            prod += genome_partial[i] * 2**abs(i[0]-len(genome_partial)+1)
+    return prod
+
 # Configure the algorithm:
 population_size = 10
-genome_length = 20
+genome_length = 2
 ga = GeneticAlgorithm(fitness_function)
 ga.generate_binary_population(size=population_size, genome_length=genome_length)
 # How many pairs of individuals should be picked to mate
